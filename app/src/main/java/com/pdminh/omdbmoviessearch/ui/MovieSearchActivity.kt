6e78cda2123f087ea849a0506c2dc6f474b3a612
Minh Pham <pdminh0410@gmail.com>
2025-03-19 10:47:07 -0700

    Support Windows Insets

--- app/src/main/java/com/pdminh/omdbmoviessearch/ui/MovieSearchActivity.kt
+++ app/src/main/java/com/pdminh/omdbmoviessearch/ui/MovieSearchActivity.kt
@@ -5,8 +5,11 @@ import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.Menu
 import android.widget.SearchView
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -34,9 +37,17 @@ class MovieSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         binding = ActivityMovieSearchBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         setupUI()
         setupObserver()
